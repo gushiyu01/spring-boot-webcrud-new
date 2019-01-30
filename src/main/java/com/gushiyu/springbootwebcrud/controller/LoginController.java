46a89d906266de21d9ef0e06883daccce8663eda
gushiyu <923686643@qq.com>
2019-01-30 11:35:31 +0800

    删除报错

--- src/main/java/com/gushiyu/springbootwebcrud/controller/LoginController.java
+++ src/main/java/com/gushiyu/springbootwebcrud/controller/LoginController.java
@@ -4,21 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
 public class LoginController {
 
     @PostMapping("/user/login")
-    public String login(String username, String password, Map map, HttpServletRequest req, HttpSession hs){
+    public String login(String username, String password, Map map){
         if(StringUtils.isEmpty(username) || "11".equals(password)){
             map.put("msg","用户名或密码错误");
             return "login";
         }
-        hs.setAttribute("token","success");
-        hs.setAttribute("username",username);
         return "redirect:/main.html";
     }
 }
