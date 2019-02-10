981dfbb8a3681d6fdc351fdce86efb8db0eb10bf
gushiyu <923686643@qq.com>
2019-02-10 23:58:51 +0800

    添加依赖

--- src/main/java/com/gushiyu/springbootwebcrud/controller/LoginController.java
+++ src/main/java/com/gushiyu/springbootwebcrud/controller/LoginController.java
@@ -1,10 +1,11 @@
 package com.gushiyu.springbootwebcrud.controller;
 
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
+
+
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
@@ -12,12 +13,11 @@ import java.util.Map;
 public class LoginController {
 
     @PostMapping(value = "/user/login")
-    public String login(String username, String password, Map<String,Object> map, HttpServletRequest req, HttpSession hs){
+    public String login(String username, String password, Map<String,Object> map, HttpSession hs){
         if(!"admin".equals(username) || !"123456".equals(password)){
             map.put("msg","用户名或密码错误");
             return "login";
         }else{
-            Cookie cookie = new Cookie("username",username);
             hs.setAttribute("token","success");
             hs.setAttribute("username",username);
             return "redirect:/main.html";
